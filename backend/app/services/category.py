822e994cbf828c6ecc9d3ff83c341bf060f729ac
nokupr1 <prodbydominus@gmail.com>
2026-07-16 11:41:43 +0700

    feat: Implement necessary services

---
+++ backend/app/services/category.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.schemas.category import CategoryCreate, CategoryResponse
+
+from ..repositories.category_repository import CategoryRepository
+
+
+class CategoryService:
+    def __init__(self, db: AsyncSession) -> None:
+        self.category_repository = CategoryRepository(db)
+
+    async def get_all(self) -> list[CategoryResponse]:
+        categories = await self.category_repository.get_all()
+        return [CategoryResponse.model_validate(category) for category in categories]
+
+    async def get_by_id(self, category_id: int) -> CategoryResponse:
+        category = await self.category_repository.get_by_id(category_id)
+        return CategoryResponse.model_validate(category)
+
+    async def get_by_slug(self, category_slug: str) -> CategoryResponse:
+        category = await self.category_repository.get_by_slug(category_slug)
+        return CategoryResponse.model_validate(category)
+
+    async def create_category(self, category_data: CategoryCreate) -> CategoryResponse:
+        category = await self.category_repository.create(category_data)
+        return CategoryResponse.model_validate(category)
